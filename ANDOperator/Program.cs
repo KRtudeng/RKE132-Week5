b52c245a46a3a7e602463f299793d526ac7bce04
Kertu Ra. <kertu.raudkivi@tktk.ee>
2024-09-06 10:05:47 +0300

    add week5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿// Math >= 90; Biology >=90; Chemistry >=90;
+
+
+int math, biology, chemistry;
+
+Console.WriteLine("enter your math resault:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology resault:");
+biology =Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry resault:");
+chemistry =Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && biology >=90 && chemistry >= 90)
+{
+    Console.WriteLine("Congartulations! You are accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
+
---
+++ OROperator/Program.cs
@@ -0,0 +1,24 @@
+﻿// Math >= 90 OR Chemistry >=90 OR 
+
+//math && chemistry || math && biology || chemistry && biology
+
+int math, biology, chemistry;
+
+Console.WriteLine("enter your math resault:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry resault:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry resault:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+if ((math >= 90 && chemistry >=90) || (math >= 90 && biology >=90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congartulations! You are accepted!");
+}
+else
+{
+    Console.WriteLine("Yor application cannot be approved.");
+}
